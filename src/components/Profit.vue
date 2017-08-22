278fa2a60868b5a29b7a141b18c89fe811cf51b3
hanguanxian <763877917@qq.com>
2017-08-23 07:14:55 +0800

    profit

--- src/components/Profit.vue
+++ src/components/Profit.vue
@@ -37,29 +37,37 @@
         </el-tab-pane>
         <el-tab-pane label="种苗">
           <el-form :inline="true" :model="tabForm2">
-            <el-form-item prop="num1" :label="蟹苗">
-              <el-input v-model="tabForm2.num1" placeholder="数量"></el-input>
-            </el-form-item>
-            <el-form-item prop="unit1">
-              <el-select v-model="tabForm2.unit1" placeholder="">
-                <el-option label="斤" :value="1"></el-option>
-                <el-option label="只" :value="0"></el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item prop="num2">
-              <el-input v-model="tabForm2.num2" placeholder="单价"></el-input>
-            </el-form-item>
-            <el-form-item prop="unit2">
-              <el-select v-model="tabForm2.unit2" placeholder="时间范围">
-                <el-option label="元/斤" :value="1"></el-option>
-                <el-option label="元/只" :value="0"></el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item prop="num1" :label="蟹苗">
-              <el-input v-model="tabForm2.num1" placeholder="数量"></el-input>
-            </el-form-item>
+            <el-row>
+              <el-form-item prop="num1" label="蟹苗">
+                <el-input v-model="tabForm2.num1" placeholder="数量"></el-input>
+              </el-form-item>
+              <el-form-item prop="unit1">
+                <el-select v-model="tabForm2.unit1" placeholder="">
+                  <el-option label="斤" :value="1"></el-option>
+                  <el-option label="只" :value="0"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item prop="num2">
+                <el-input v-model="tabForm2.num2" placeholder="单价"></el-input>
+              </el-form-item>
+              <el-form-item prop="unit2">
+                <el-select v-model="tabForm2.unit2" placeholder="单位">
+                  <el-option label="元/斤" :value="1"></el-option>
+                  <el-option label="元/只" :value="0"></el-option>
+                </el-select>
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <div style="display: inline;">套养</div>
+              <el-form-item prop="num3" label="鱼">
+                <el-input v-model="tabForm2.num3" placeholder="金额"></el-input>
+              </el-form-item>
+              <el-form-item prop="num1" label="虾">
+                <el-input v-model="tabForm2.num4" placeholder="金额"></el-input>
+              </el-form-item>
+            </el-row>
             <el-form-item>
-              <el-button type="primary" @click="">查询数据</el-button>
+              <el-button type="primary" @click="">计算</el-button>
             </el-form-item>
           </el-form>
         </el-tab-pane>
@@ -86,7 +94,13 @@
           unit1: '',
           num2: '',
           unit2: ''
-        }
+        },
+        tabForm2:{
+          num1: '',
+          unit1: '',
+          num2: '',
+          unit2: ''
+        },
       }
     },
     created() {
